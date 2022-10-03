cdef3f84865c36b84941e159a6d40fcdbd8c87a7
Flynn P O'Connell <oconnell@binghamton.edu>
2022-10-03 18:07:36 -0400

    Added to .gitignore, license

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup
 setup(
         name='web_graph_testing',
         version='0.0.1',
-        packages=[''],
+        packages=['web_graph_testing'],
         url='git@github.com:NeuroPyPy/web_graph_testing.git',
-        license='',
-        author='flynn',
-        author_email='',
-        description=''
+        license='MIT',
+        author='Flynn OConnell',
+        author_email='oconnell@binghamton.edu',
+        description='Testing some web/graphical applications and interfaces.',
+        install_requires=['flask, plotly']
 )
