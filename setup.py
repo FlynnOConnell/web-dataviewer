fa2af9d92039ed45debca694d5be7392616213a7
Flynn P O'Connell <oconnell@binghamton.edu>
2022-10-03 17:41:30 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+        name='web_graph_testing',
+        version='0.0.1',
+        packages=[''],
+        url='git@github.com:NeuroPyPy/web_graph_testing.git',
+        license='',
+        author='flynn',
+        author_email='',
+        description=''
+)
