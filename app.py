c01f139a443c77f0f2cf13a5876e67a220557612
Flynn P O'Connell <oconnell@binghamton.edu>
2022-10-03 17:30:12 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,60 @@
+"""
+ Here I define the different routes for the website and specify the settings.
+  The default route shows the index.html where a canvas is shown.
+  The result route will show the image once a picture is drawn, based on the provided unique ID.
+   The postmethod route is defined to handle the data coming from Javascript into Python via a POST call.
+    The content of the POST variable are written to a CSV file which can be used again on the result page where data is loaded from this same file.
+"""
+
+from __future__ import print_function
+from flask import Flask, render_template, make_response
+from flask import redirect, request, jsonify, url_for
+import json
+import io
+import os
+import base64
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import numpy as np
+
+
+app = Flask(__name__)
+app.secret_key = 's3cr3t'
+app.debug = True
+app._static_folder = os.path.abspath("static/")
+
+
+def convert_color(color_arr: np.ndarray):
+    import matplotlib.colors as mplcolors
+    assert (mplcolors.is_color_like(color_arr))
+
+
+@app.route('/', methods=['GET'])
+def home():
+    title = 'Draw something!'
+    return render_template(
+            'home.html',
+            title=title,
+    )
+
+
+# @app.route('/postmethod', methods = ['POST'])
+# def post_javascript_data():
+#     jsdata = request.form['canvas_data']
+#     fig = Figure()
+#     floats = np.asarray(json.loads(jsdata)).reshape(200, 200)
+#     axis = fig.add_subplot(1, 1, 1)
+#     axis.axis('off')
+#     axis.imshow(floats)
+#     canvas = FigureCanvas(fig)
+#     output = io.BytesIO()
+#     canvas.print_png(output)
+#     encoded = base64.b64encode(output.getvalue())
+#     response = make_response(encoded)
+#     response.mimetype = 'image/png'
+#     return response
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
---
+++ static/js/script.js
@@ -0,0 +1,82 @@
+//  In this file a canvas is generated and added to the DOM.
+//  The mouse is used to draw dots on the canvas with a predefined color and radius.
+//  One button is used to send the data of the current drawing on the canvas and another one is used to clear the canvas.
+
+
+$( document ).ready(function() {
+
+   function createCanvas() {
+    var canvas = document.getElementById("inputCanvas");
+    canvas.context = canvas.getContext('2d');
+    return canvas;
+  }
+
+  function init() {
+    var canvas = createCanvas();
+    var ctx = canvas.context;
+    ctx.fillCircle = function(x, y, radius, drawColor) {
+      this.fillStyle = drawColor;
+      this.beginPath();
+      this.moveTo(x, y);
+      this.arc(x, y, radius, 0, Math.PI * 2, false);
+      this.fill();
+    };
+    ctx.clearTo = function(fillColor) {
+      ctx.fillStyle = fillColor;
+      ctx.fillRect(0, 0, ctx.width, ctx.height);
+    };
+    ctx.clearTo(fillColor);
+
+    canvas.onmousemove = function(e) {
+      if (!canvas.isDrawing) {
+        return;
+      }
+      var x = e.pageX - this.offsetLeft;
+      var y = e.pageY - this.offsetTop;
+      var radius = 10;
+      var fillColor = drawColor;
+      ctx.fillCircle(x, y, radius, drawColor);
+    };
+    canvas.onmousedown = function(e) {
+      canvas.isDrawing = true;
+    };
+    canvas.onmouseup = function(e) {
+      canvas.isDrawing = false;
+    };
+  }
+
+  var container = document.getElementById('canvas');
+  init(container);
+  var fillColor = "#ddd"
+  var drawColor = 'rgb(163,33,33)';
+
+  function clearCanvas() {
+    var canvas = document.getElementById("inputCanvas");
+    var ctx = canvas.getContext("2d");
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+
+  function getData() {
+    var canvas = document.getElementById("inputCanvas");
+    var imageData = canvas.context.getImageData(0, 0, canvas.width, canvas.height);
+    var data = imageData.data;
+    var outputData = []
+    for (var i = 0; i < data.length; i += 4) {
+      var brightness = 0.34 * data[i] + 0.5 * data[i + 1] + 0.16 * data[i + 2];
+      outputData.push(brightness);
+    }
+    $.post("/postmethod", {
+      canvas_data: JSON.stringify(outputData)
+    }, function (err, req, resp) {
+      $("#graph").attr("src", "data:image/png;base64," + resp.responseText);
+    });
+  }
+
+  $( "#clearButton" ).click(function(){
+    clearCanvas();
+  });
+
+  $( "#sendButton" ).click(function(){
+    getData();
+  });
+});
